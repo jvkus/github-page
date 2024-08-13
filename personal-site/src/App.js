2d7d1d5533bf7da514e7056e0a846f36a6aa305e
jvkus <joannavkus@gmail.com>
2024-08-13 17:49:29 -0500

    feat: add basic ideas for website

---
+++ personal-site/src/App.js
@@ -0,0 +1,87 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hi
+          I'm Joanna Kus
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This website is in progress. Pardon the dust.
+        </a>
+      </header>
+      <body>
+        I'm a software engineer with 3 years of experience.
+
+        Contacts here. List email but hide it so robots can't scrape it.
+
+        Put timeline here of past experience.
+
+        Now - Icons
+          - Building new things
+          - Contributing to what open source projects?
+
+        3 years at RetailMeNot - Icons of programming languages used
+          - Some bullets about what I did
+
+        Graduated from Elmhurst University
+          - University projects. links. screenshots?
+
+        iD Tech experience 3 - Programming languages
+          - bullets
+
+        iD Tech experience 2
+          - bullets
+
+        iD Tech experience 1
+          - bullets 
+
+        Tech Stack
+        PHP
+        React.js
+        Python
+        Java
+
+        Off the Clock
+
+        List of interests here
+
+        <a
+          href="https://example.com"
+        >
+          Resume
+        </a>
+        <a
+          href="https://example.comhttps://www.linkedin.com/in/joanna-kus-330724162/"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/jvkus"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://infosec.exchange/@jvkus"
+        >
+          Mastodon
+        </a>
+        <a
+          href="https://example.com"
+        >
+          Blog link (Coming soon)
+        </a>
+      </body>
+    </div>
+  );
+}
+
+export default App;
